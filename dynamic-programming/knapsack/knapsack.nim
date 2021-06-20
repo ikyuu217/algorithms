9c74770fd7dcbd0ecdca81b1f430c6244a0e66e7
ikyuu217 <61170826+ikyuu217@users.noreply.github.com>
2021-06-20 18:37:42 +0900

    Create knapsack.nim

---
+++ dynamic-programming/knapsack/knapsack.nim
@@ -0,0 +1,36 @@
+import sequtils,strutils
+import sugar
+
+iterator range(n: int): int =
+  for i in 0..<n:
+    yield i
+
+proc getLine[T](fun: string -> T): seq[T] =
+  stdin.readLine.split.map fun
+
+# input:
+#   N: num of items
+#   W: capacity of the bag
+#   [V]: value of each item 
+#   [Wei]: weight of each item
+# task:
+#   maximize: ...
+#   subject to: ...
+
+block main:
+  # input
+  var
+     N,W: int
+  (N,W) = getLine(parseInt)
+  var V, Wei = newSeq[int](N)
+  for i in range(N): 
+    (V[i],Wei[i]) = getLine(parseInt)
+  
+  # solve
+  var K = newSeq[int](W+1)
+  for w in 1..W:
+    for i in range(N):
+      if w - Wei[i] >= 0:
+        K[w] = max(K[w], K[w-Wei[i]] + V[i])
+  
+  K[W].echo
